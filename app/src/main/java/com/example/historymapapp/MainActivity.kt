bc590669f5c14142facd49c4ad54b36d43a15202
Vladyslav Papanov <vladpapanov3@gmail.com>
2025-03-26 21:50:24 +0100

    Initial commit

---
+++ app/src/main/java/com/example/historymapapp/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.example.historymapapp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import org.osmdroid.config.Configuration
+import org.osmdroid.views.MapView
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory
+import org.osmdroid.util.GeoPoint
+import org.osmdroid.views.overlay.Marker
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var map: MapView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        Configuration.getInstance()
+            .load(applicationContext, getSharedPreferences("osmdroid", MODE_PRIVATE))
+
+        setContentView(R.layout.activity_main)
+
+        map = findViewById(R.id.map)
+        map.setTileSource(TileSourceFactory.MAPNIK)
+        map.controller.setZoom(10.0)
+        map.controller.setCenter(GeoPoint(52.2298, 21.0122)) // Warszawa
+
+        val marker = Marker(map).apply {
+            position = GeoPoint(52.2298, 21.0122)
+            title = "Warszawa"
+        }
+
+        map.overlays.add(marker)
+    }
+}
